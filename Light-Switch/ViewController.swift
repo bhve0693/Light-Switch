5bd2613411f5816b2b7a3cdd7b9c3b6b728ccc57
bhallaji <bhallaji@bhallaji.local>
2018-11-03 14:10:54 -0700

    Initial Commit

---
+++ Light-Switch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Light-Switch
+//
+//  Created by bhallaji on 03/11/18.
+//  Copyright © 2018 bhallaji. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
